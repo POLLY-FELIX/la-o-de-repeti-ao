18c8d99cdcad9b7016c1894b29213b57187ddcc7
Aluno <Aluno@DESKTOP-SANL2T8>
2024-01-23 20:53:08 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laço_de_repetiçao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int numero = 0;
+
+            //Enquanto a condição for verdadeira, repita
+            string resposta = "";
+
+            while (resposta != "sim")
+            {
+                while (numero <= 1000)
+                {
+                    Console.WriteLine($"O número é: {numero}");
+                    numero++;
+
+                }
+                numero = 0;
+
+                Console.WriteLine("deseja para o programa?");
+                resposta = Console.ReadLine();
+            }
+         }
+    }
+}
